fe07dc842d74201ffab35c8efc76346980077923
Oliver Vogel <oliver@olivervogel.com>
2020-02-20 19:30:34 +0100

    Changed test

--- tests/HttpAuthTest.php
+++ tests/HttpAuthTest.php
@@ -2,7 +2,7 @@
 
 namespace Intervention\HttpAuth\Test;
 
-use Intervention\HttpAuth\HttpAuth;
+use Intervention\HttpAuth\HttpAuth as Auth;
 use Intervention\HttpAuth\Vault\BasicVault;
 use Intervention\HttpAuth\Vault\DigestVault;
 use PHPUnit\Framework\TestCase;
@@ -11,36 +11,36 @@ class HttpAuthTest extends TestCase
 {
     public function testMake()
     {
-        $this->assertInstanceOf(BasicVault::class, HttpAuth::make());
-        $this->assertInstanceOf(DigestVault::class, HttpAuth::make(['type' => 'digest']));
+        $this->assertInstanceOf(BasicVault::class, Auth::make());
+        $this->assertInstanceOf(DigestVault::class, Auth::make(['type' => 'digest']));
     }
 
     public function testBasic()
     {
-        $this->assertInstanceOf(BasicVault::class, HttpAuth::basic());
+        $this->assertInstanceOf(BasicVault::class, Auth::basic());
     }
 
     public function testDigest()
     {
-        $this->assertInstanceOf(DigestVault::class, HttpAuth::digest());
+        $this->assertInstanceOf(DigestVault::class, Auth::digest());
     }
 
     public function testMagicCall()
     {
-        $vault = HttpAuth::username('foo');
+        $vault = Auth::username('foo');
         $this->assertInstanceOf(BasicVault::class, $vault);
         $this->assertEquals('foo', $vault->getUsername());
     }
 
     public function testCallConfiguration()
     {
-        $vault = HttpAuth::digest()->username('foo')->password('bar')->realm('baz');
+        $vault = Auth::digest()->username('foo')->password('bar')->realm('baz');
         $this->assertInstanceOf(DigestVault::class, $vault);
         $this->assertEquals('foo', $vault->getUsername());
         $this->assertEquals('bar', $vault->getPassword());
         $this->assertEquals('baz', $vault->getRealm());
 
-        $vault = HttpAuth::digest();
+        $vault = Auth::digest();
         $vault->username('foo');
         $vault->password('bar');
         $vault->realm('baz');
