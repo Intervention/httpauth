43a445e3965dfac76424eecf884dbe54e3ea0954
Oliver Vogel <oliver@olivervogel.com>
2024-05-02 17:30:31 +0200

    Fix wrong namespace

--- tests/Unit/Token/AbstractTokenTest.php
+++ tests/Unit/Token/AbstractTokenTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Intervention\HttpAuth\Tests\Unit\Token;
 
-use Intervention\HttpAuth\Test\AbstractTokenTestCase;
+use Intervention\HttpAuth\Tests\AbstractTokenTestCase;
 use Intervention\HttpAuth\Token\AbstractToken;
 use Intervention\HttpAuth\Key;
 
